9aa6d6a41a7c9c95e812bf213a20b886075e8b70
Vinh124567 <phungducvinh1245@gmail.com>
2023-10-01 22:27:44 +0700

    edit sql

--- qldoantn.sql
+++ qldoantn.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 29, 2023 at 07:43 PM
+-- Generation Time: Oct 01, 2023 at 05:25 PM
 -- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -39,10 +39,35 @@ CREATE TABLE `class` (
 --
 
 INSERT INTO `class` (`id`, `name`, `faculty_id`, `course_id`) VALUES
-(1, 'HT22', 1, 2),
-(2, 'HT23', 8, 3),
-(3, 'DH8QM1', 1, 1),
+(1, 'DH8C1', 1, 1),
+(2, 'DH8C2', 1, 1),
+(3, 'DH8QM1', 3, 1),
 (4, 'DH9C1', 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `council`
+--
+
+CREATE TABLE `council` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `teacher_id1` int(11) DEFAULT NULL,
+  `teachers_id2` int(11) DEFAULT NULL,
+  `teachers_id3` int(11) DEFAULT NULL,
+  `star_date` date DEFAULT NULL,
+  `star_time` time DEFAULT NULL,
+  `rooms` varchar(255) DEFAULT NULL,
+  `academic_year` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `council`
+--
+
+INSERT INTO `council` (`id`, `name`, `teacher_id1`, `teachers_id2`, `teachers_id3`, `star_date`, `star_time`, `rooms`, `academic_year`) VALUES
+(1, 'HDMI', 1, 1, 1, '1111-11-11', '00:00:11', '11', '2018-2022');
 
 -- --------------------------------------------------------
 
@@ -62,9 +87,32 @@ CREATE TABLE `course` (
 --
 
 INSERT INTO `course` (`id`, `name`, `time`, `note`) VALUES
-(1, 'HTTT', '2018-2022', 'Hệ thống thông tin'),
-(2, 'TMDT', '2019-2023', 'Thương Mại Điện Tử\r\n'),
-(3, 'KT', '2020-2025', 'Kinh Tế');
+(1, 'DH8', '2018-2022', 'khóa 2000'),
+(2, 'DH9', '2019-2023', 'đã xem');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dsdetai`
+--
+
+CREATE TABLE `dsdetai` (
+  `id` int(11) NOT NULL,
+  `tenDT` varchar(255) NOT NULL,
+  `thoiGianDB` date NOT NULL,
+  `thoiGianKT` date NOT NULL,
+  `nguoiThuchien` varchar(255) NOT NULL,
+  `giangVienhd` varchar(255) NOT NULL,
+  `motaDT` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `dsdetai`
+--
+
+INSERT INTO `dsdetai` (`id`, `tenDT`, `thoiGianDB`, `thoiGianKT`, `nguoiThuchien`, `giangVienhd`, `motaDT`) VALUES
+(1, 'lập trình', '2023-10-12', '2024-03-15', 'hunh', 'vinh', 'làm đồ án'),
+(12, 'lập trình', '0000-00-00', '0000-00-00', '2fhisfa', 'asfa', '');
 
 -- --------------------------------------------------------
 
@@ -86,24 +134,11 @@ CREATE TABLE `faculty` (
 --
 
 INSERT INTO `faculty` (`id`, `code`, `name`, `phone`, `birthday`, `note`) VALUES
-(1, 'CNTT', 'Công nghệ thông tin', '0972103238', '1111-11-11', ''),
+(1, 'IT', 'Công nghệ thông tin', '12312', '1111-11-11', 'Khoa IT nhiều trai'),
+(3, 'HEVT', 'Môi trường', '03662804402', '2012-11-02', 'LÃ  khoa chá»§ chá»‘t'),
 (5, '12', 'Kinh tế', '12', '2111-12-12', '12'),
-(8, 'KTVT', 'Kinh tế vận tải', '03365697', '2003-11-23', ''),
-(10, 'CK', 'Cơ khí', '033652214', '1990-11-11', 'Khoa cơ khí');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `hoidong`
---
-
-CREATE TABLE `hoidong` (
-  `id` int(11) NOT NULL,
-  `tenhoidong` varchar(50) NOT NULL,
-  `teachers` varchar(50) NOT NULL,
-  `falcuty` varchar(50) NOT NULL,
-  `ghichu` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+(6, 'EV', 'Đất đai', '0988910811', '0002-12-31', '12'),
+(8, 'EV', 'ngodinhloc', '12', '1111-11-11', '12312');
 
 -- --------------------------------------------------------
 
@@ -124,9 +159,23 @@ CREATE TABLE `login` (
 --
 
 INSERT INTO `login` (`id`, `username`, `password`, `fullname`, `avatar`) VALUES
-(1, 'test', 'd0521106f6ba7f9ac0a7370fb28d0ec6', 'trưởng khoa', 'mai1.jpg'),
+(1, 'test', '21232f297a57a5a743894a0e4a801fc3', 'trưởng khoa', ''),
 (2, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Admin', ''),
-(5, 'admin1', '14e1b600b1fd579f47433b88e8d85291', 'Mai', NULL);
+(3, 'locdybala11', '3a2847382a7b363139b5eb0f863a62ff', 'Ngô Đình Lộc', '262132193_383898550039048_309104012239693736_n.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nienkhoa`
+--
+
+CREATE TABLE `nienkhoa` (
+  `id` int(11) NOT NULL,
+  `maNienkhoa` varchar(255) NOT NULL,
+  `tgbatdau` year(4) NOT NULL,
+  `tgketthuc` year(4) NOT NULL,
+  `malop` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -151,9 +200,8 @@ CREATE TABLE `project` (
 --
 
 INSERT INTO `project` (`id`, `title`, `content`, `student_id`, `teacher_id`, `code_project`, `point`, `linkdownload`, `file`) VALUES
-(1, 'Quản lý thời trang nữ', '', 2, 3, 'ĐA01', 10, 'https://github.com/locdybala/bandienmay.git', 'FTU_Gioi thieu - Chuong 1.PDF'),
-(2, 'Quản lý đồ án', '', 2, 3, '1212', 9, 'https://www.google.com.vn/drive/about.html', ''),
-(5, 'Quản lí SV', '', 2, 3, 'ĐA03', 9, 'https://www.google.com/search?q=running+man+vietsub+ep+673&sca_esv=568507283&sxsrf=AM9HkKnKR8zfiBNVI', 'FTU_Gioi thieu - Chuong 1.PDF');
+(1, 'Quản lý thời trang nữ', '<p><strong>Cuộc đời n&agrave;y l&agrave; m&agrave;u hồng</strong></p>\r\n', 4, 2, 'ĐA01', 10, 'https://github.com/locdybala/bandienmay.git', '123.PDF'),
+(2, 'Quản lý đồ án', '<p>test th&ecirc;m</p>\r\n\r\n<p>&nbsp;</p>\r\n', 2, 2, '1212', 9, 'https://www.google.com.vn/drive/about.html', '');
 
 -- --------------------------------------------------------
 
@@ -177,9 +225,9 @@ CREATE TABLE `student` (
 --
 
 INSERT INTO `student` (`id`, `code`, `name`, `birthday`, `email`, `phone`, `address`, `class_id`) VALUES
-(2, '20051', 'Nguyễn Thị Thanh Mai', '1111-11-20', '0111@gmail.com', 366280440, '11112312', 1),
-(6, '20052', 'Nguyễn Thị Mai', '1995-11-23', 'mai@gmail.com', 336572367, 'BN', 1),
-(7, '20053', 'Nguyễn Văn A', '2003-12-26', 'email@email.com', 325631456, 'Hà Nội', 1);
+(2, '21312412', 'Pháº¡m Thá» TÃº TÃ i', '1111-11-20', '0111@gmail.com', 366280440, '11112312', 1),
+(3, '123123122', 'Nguyá»…n VÄƒn KhÃ¡nh', '1111-11-02', 'locnd@softdreams.vnsadsadasassa', 12312312, '123124', 2),
+(4, '1811060337', 'NgÃ´ ÄÃ¬nh Lá»™c', '2000-11-10', '', 366280440, 'PhÃ¹ng XÃ¡ Tháº¡ch Tháº¥t HÃ  Ná»™i', 3);
 
 -- --------------------------------------------------------
 
@@ -203,9 +251,9 @@ CREATE TABLE `teachers` (
 --
 
 INSERT INTO `teachers` (`id`, `name`, `birthday`, `phone`, `address`, `email`, `avatar`, `faculty_id`) VALUES
-(1, 'Nguyễn Thị Thanh Mai', '1995-11-23', '12312312', '111222', '11112312', 'mai1.jpg', 5),
+(1, 'Sá»­a Ä‘Æ°á»£c rá»“i nÃ y', '1111-11-11', '12312312', '111222', '11112312', 'FD51544F-30F6-429A-9AFD-7323AAB01317.png', 5),
 (2, 'ManageUseronPhalcon', '1111-11-11', '11111', '123124', 'locdz2000@gmail.com', 'image_2022_08_01T04_32_09_527Z.png', 3),
-(3, 'Bùi Thị Xuân Hương', '1995-01-25', '0366280440', 'Hà Nội', 'locdybala11@gmail.com', 'z4422484803180_6b0ad24abd6579923bf84b17aa93071c.jpg', 1);
+(3, 'LÃª PhÃº HÆ°ng', '1111-11-11', '0366280440', 'HÃ  Ná»™i', 'locdybala11@gmail.com', 'image_2022_08_01T04_32_09_527Z.png', 1);
 
 --
 -- Indexes for dumped tables
@@ -218,9 +266,21 @@ ALTER TABLE `class`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `council`
+--
+ALTER TABLE `council`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `course`
 --
 ALTER TABLE `course`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `dsdetai`
+--
+ALTER TABLE `dsdetai`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -230,15 +290,15 @@ ALTER TABLE `faculty`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `hoidong`
---
-ALTER TABLE `hoidong`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `login`
 --
 ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `nienkhoa`
+--
+ALTER TABLE `nienkhoa`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -267,43 +327,55 @@ ALTER TABLE `teachers`
 -- AUTO_INCREMENT for table `class`
 --
 ALTER TABLE `class`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `council`
+--
+ALTER TABLE `council`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `course`
 --
 ALTER TABLE `course`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `dsdetai`
+--
+ALTER TABLE `dsdetai`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `faculty`
 --
 ALTER TABLE `faculty`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT for table `hoidong`
---
-ALTER TABLE `hoidong`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `nienkhoa`
+--
+ALTER TABLE `nienkhoa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `project`
 --
 ALTER TABLE `project`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `teachers`
